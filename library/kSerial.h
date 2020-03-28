307cb61655dcd9eb3dca9f586b2ef931a2bf075e
Krystian Dużyński <krystian.duzynski@gmail.com>
2020-03-28 20:11:57 +0100

    added kCommon and added more functions to kSerial

--- library/kSerial.h
+++ library/kSerial.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <kCommon.h>
+
 #include <avr/io.h>
+#include <avr/common.h>
 
 #include <stdint.h>
 
@@ -9,8 +12,29 @@ class kSerial
 public:
 	static void put(char c)
 	{
-		while (bit_is_clear (UCSRA, UDRE));
+		while (bit_is_clear(UCSRA, UDRE));
 		UDR = c;
 	}
+
+	static char get()
+	{
+		while (bit_is_clear(UCSRA, RXC));
+		return UDR;
+	}
+
+	static void enableTx() { sbi(UCSRB, TXEN); }
+	static void disableTx() { cbi(UCSRB, TXEN); }
+
+	static void enableRx() { sbi(UCSRB, RXEN); }
+	static void disableRx() { cbi(UCSRB, RXEN); }
+
+	static void enableTxCompletionInterrupt() { sbi(UCSRB, TXCIE); }
+	static void disableTxCompletionInterrupt() { cbi(UCSRB, TXCIE); }
+
+	static void enableRxCompletionInterrupt() { sbi(UCSRB, RXCIE); }
+	static void disableRxCompletionInterrupt() { cbi(UCSRB, RXCIE); }
+
+	static void enableRxDataReadyInterrupt() { sbi(UCSRB, UDRE); }
+	static void disableRxDataReadyInterrupt() { cbi(UCSRB, UDRE); }
 };
 
