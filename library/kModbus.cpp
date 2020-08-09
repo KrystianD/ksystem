0c98d84dad4411e142ca018dd7c78cc4928ac14c
Krystian Dużyński <krystian.duzynski@gmail.com>
2020-08-09 04:27:44 +0200

    modbus: conditionally generate handler functions

--- library/kModbus.cpp
+++ library/kModbus.cpp
@@ -3,6 +3,8 @@
 #include <mb.h>
 #include <mbport.h>
 
+#include <ksystem_internal.h>
+
 uint16_t swapU16(uint16_t val)
 {
 	return (val << 8) | ((val >> 8) & 0xFF);
@@ -25,6 +27,7 @@ void kModbus::process()
 	eMBPoll();
 }
 
+#if defined(MB_FUNC_READ_INPUT_ENABLED)
 eMBErrorCode eMBRegInputCB(UCHAR* pucRegBuffer, USHORT usAddress, USHORT usNRegs)
 {
 	uint16_t* wordPtr = (uint16_t*)pucRegBuffer;
@@ -43,12 +46,15 @@ eMBErrorCode eMBRegInputCB(UCHAR* pucRegBuffer, USHORT usAddress, USHORT usNRegs
 
 	return MB_ENOERR;
 }
+#endif
 
+#if defined(MB_FUNC_READ_HOLDING_ENABLED) || defined(MB_FUNC_WRITE_HOLDING_ENABLED)
 eMBErrorCode eMBRegHoldingCB(UCHAR* pucRegBuffer, USHORT usAddress, USHORT usNRegs, eMBRegisterMode eMode)
 {
 	uint16_t* wordPtr = (uint16_t*)pucRegBuffer;
 
 	if (eMode == MB_REG_READ) {
+#ifdef MB_FUNC_READ_HOLDING_ENABLED
 		for (uint16_t i = 0; i < usNRegs;) {
 			ModbusReturnValue value = { .buffer = &wordPtr[i], .pIdx=&i };
 			bool res = modbusHandleReadHoldingRegister(usAddress + i, value);
@@ -59,22 +65,28 @@ eMBErrorCode eMBRegHoldingCB(UCHAR* pucRegBuffer, USHORT usAddress, USHORT usNRe
 			if (!res)
 				return MB_ENOREG;
 #endif
+#endif
 		}
 	}
 	else {
+#ifdef MB_FUNC_WRITE_HOLDING_ENABLED
 		for (uint16_t i = 0; i < usNRegs; i++) {
 			uint16_t value = swapU16(wordPtr[i]);
 			if (!modbusHandleWriteHoldingRegister(usAddress + i, ModbusValue::U16(value)))
 				return MB_ENOREG;
 		}
+#endif
 	}
 
 	return MB_ENOERR;
 }
+#endif
 
+#if defined(MB_FUNC_READ_COILS_ENABLED) || defined(MB_FUNC_WRITE_COIL_ENABLED)
 eMBErrorCode eMBRegCoilsCB(UCHAR* pucRegBuffer, USHORT usAddress, USHORT usNCoils, eMBRegisterMode eMode)
 {
 	if (eMode == MB_REG_READ) {
+#ifdef MB_FUNC_READ_COILS_ENABLED
 		for (uint16_t i = 0; i <= i / 8; i++)
 			pucRegBuffer[i] = 0;
 
@@ -92,15 +104,19 @@ eMBErrorCode eMBRegCoilsCB(UCHAR* pucRegBuffer, USHORT usAddress, USHORT usNCoil
 					break;
 			}
 		}
+#endif
 	}
 	else {
+#ifdef MB_FUNC_WRITE_COIL_ENABLED
 		for (uint16_t i = 0; i < usNCoils; i++)
 			if (!modbusHandleWriteCoil(usAddress + i, pucRegBuffer[i / 8] & (1 << (i % 8))))
 				return MB_ENOREG;
+#endif
 	}
 
 	return MB_ENOERR;
 }
+#endif
 
 //eMBErrorCode eMBRegDiscreteCB(UCHAR* pucRegBuffer, USHORT usAddress, USHORT usNDiscrete)
 //{
